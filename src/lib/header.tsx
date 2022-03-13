f8fbda21e036497218556e02512a1e260f8661b4
potof <slptmac@gmail.com>
2022-03-13 23:50:01 +0900

    update image link

--- src/lib/header.tsx
+++ src/lib/header.tsx
@@ -1,6 +1,7 @@
 // import Link from "next/link";
 import { Box } from "@chakra-ui/react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
@@ -14,12 +15,16 @@ const Header = () => {
         w="full"
         bg="#019cd0"
       >
-        <Image
-          src="/logo_clear2.png"
-          alt="ぽとふバーガーDX"
-          height={62}
-          width={378}
-        ></Image>
+        <Link href="/">
+          <a>
+            <Image
+              src="/logo_clear2.png"
+              alt="ぽとふバーガーDX"
+              height={62}
+              width={378}
+            ></Image>
+          </a>
+        </Link>
       </Box>
     </>
   );
