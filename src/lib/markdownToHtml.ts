75a55b102378751e81ac79bddf2bb60efdb5ccb9
potof <slptmac@gmail.com>
2022-03-14 08:43:47 +0900

    update remark をやめて unified + rehype へ

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,16 +1,14 @@
-import { remark } from "remark";
-import html from "remark-html";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remark2rehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
 
-/**
- * Markdown を解析して HTML にして返す
- * @param markdown Markdown ファイル名
- * @returns HTML
- */
-const markdownToHtml = async (markdown: string) => {
-  const result = await remark()
-    .use(html, { sanitize: false })
-    .process(markdown);
+// Markdown 形式の文字列を受け取って、HTML 形式の文字列を返す
+export default async function markdownToHtml(markdown: string) {
+  const result = await unified()
+    .use(remarkParse) // markdown -> mdast の変換
+    .use(remark2rehype) // mdast -> hast の変換
+    .use(rehypeStringify) // hast -> html の変換
+    .process(markdown); // 実行
   return result.toString();
-};
-
-export default markdownToHtml;
+}
