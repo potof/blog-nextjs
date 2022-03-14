a87a716c4faa050055dfd99f9fa86ede4b588cd9
potof <slptmac@gmail.com>
2022-03-15 08:15:18 +0900

    update rename

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "./Theme";
+import theme from "../lib/theme";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
