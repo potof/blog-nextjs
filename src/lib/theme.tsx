4694075bdd49e7ac97bc239853c8eec8ed021a30
potof <slptmac@gmail.com>
2022-03-15 08:14:48 +0900

    update rename

--- src/lib/theme.tsx
+++ src/lib/theme.tsx
@@ -1,6 +1,6 @@
 import { extendTheme } from "@chakra-ui/react";
 
-export const theme = extendTheme({
+const theme = extendTheme({
   styles: {
     global: {
       body: {
@@ -10,3 +10,4 @@ export const theme = extendTheme({
     },
   },
 });
+export default theme;
