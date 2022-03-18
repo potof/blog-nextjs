ac5200821e7ad651c92f5c5b4a2ed4b285f7cdef
potof <slptmac@gmail.com>
2022-03-18 09:39:31 +0900

    update: log and copyright

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,5 @@
-import { Box } from "@chakra-ui/react";
+import Link from "next/link";
+import { Box, Text, Stack, Image } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
@@ -6,13 +7,26 @@ const Footer = () => {
       <Box
         display="flex"
         alignItems="center"
+        justifyContent="center"
         minHeight="10vh"
-        gap={8}
-        mb={8}
         w="full"
         bg="#019cd0"
       >
-        This is Footer
+        <Stack p={2} align="center">
+          <Link href="/">
+            <a>
+              <Image
+                src="/logo_clear2.png"
+                alt="ぽとふバーガーDX"
+                height={50}
+                width={302.88}
+              ></Image>
+            </a>
+          </Link>
+          <Text pt={6} fontSize={"sm"} textAlign={"center"} color="gray.100">
+            © 2022 ぽとふバーガーDX All rights reserved.
+          </Text>
+        </Stack>
       </Box>
     </>
   );
