a6223f10ca92d91dbd56521d0bcde6ab9b59b1ce
potof <slptmac@gmail.com>
2022-03-21 09:35:37 +0900

    fix: delete undefined module

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -5,7 +5,6 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { getAllPosts, getPostBySlug } from "../lib/api";
-import markdownToHtml from "../lib/markdownToHtml";
 import {
   Box,
   Heading,
