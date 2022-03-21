ee60347ddda8a63f9485c8ebe6fd5741a830bfc1
potof <slptmac@gmail.com>
2022-03-21 22:44:31 +0900

    add: sns link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
-import { Box, Text, Stack, Image } from "@chakra-ui/react";
+import { Box, Stack, Image, Text, Link as UILink } from "@chakra-ui/react";
 import Link from "next/link";
+import { FaTwitter, FaGithub, FaBook } from "react-icons/fa";
 
 const Header = () => {
   return (
@@ -21,9 +22,40 @@ const Header = () => {
                 height={62}
                 width={378}
                 _hover={{ opacity: 0.5 }}
+                display="inline"
               ></Image>
             </a>
           </Link>
+          <Text>
+            <UILink
+              href="https://twitter.com/Potof_"
+              _hover={{ opacity: 0.5 }}
+              isExternal
+            >
+              <FaTwitter
+                size={30}
+                color={"#fff"}
+                style={{
+                  display: "inline",
+                  marginRight: "7px",
+                }}
+              />
+            </UILink>
+            <UILink
+              href="https://bookmeter.com/users/117579"
+              _hover={{ opacity: 0.5 }}
+              isExternal
+            >
+              <FaBook
+                size={30}
+                color={"#fff"}
+                style={{
+                  display: "inline",
+                  marginRight: "3px",
+                }}
+              />
+            </UILink>
+          </Text>
 
           {/* <Text color="white">About</Text> */}
         </Stack>
