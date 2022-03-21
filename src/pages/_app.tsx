1cb6c9c6f611f5aff67888a78b20cf1245d51833
potof <slptmac@gmail.com>
2022-03-21 21:26:41 +0900

    add: google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import GoogleAnalytics from "../lib/gtag";
 import theme from "../styles/theme";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <GoogleAnalytics />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 };
 
