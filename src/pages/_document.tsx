79b1a6c07d2e9ac564fe380d7e01572cd55b93b1
potof <slptmac@gmail.com>
2022-03-24 08:28:33 +0900

    update: ogp tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,25 +1,35 @@
-import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
-type Props = {};
+const MyDocument = () => {
+  const url = "https://potof.net";
+  const title = "ぽとふバーガーDX | 日記";
+  const description =
+    "エンジニアが自分のために旅行や買い物を記録していくブログ";
 
-class Document extends NextDocument<Props> {
-  render() {
-    return (
-      <Html lang="ja">
-        <Head>
-          <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1536057836501643"
-            crossOrigin="anonymous"
-          ></script>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+  return (
+    <Html lang="ja-JP">
+      <Head>
+        <meta name="description" content={description} />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content={title} />
+        <meta property="og:url" content={url} />
+        <meta property="og:description" content={description} />
+        <meta property="og:site_name" content={title} />
+        <meta property="og:image" content={`${url}/logo_clear2.png`} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="format-detection" content="telephone=no" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1536057836501643"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
 
-export default Document;
+export default MyDocument;
